11a3d2a9e5fa5687b0ff77eca886a1aa664a1941
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2009-11-29 20:34:16 +0000

    initial contribution for OCL model checker

---
+++ plugins/org.eclipse.emf.henshin.statespace.ocl/src/org/eclipse/emf/henshin/statespace/ocl/OCLModelChecker.java
@@ -0,0 +1,27 @@
+package org.eclipse.emf.henshin.statespace.ocl;
+
+import java.text.ParseException;
+
+import org.eclipse.emf.henshin.statespace.StateSpace;
+import org.eclipse.emf.henshin.statespace.modelchecking.ModelChecker;
+
+/**
+ * OCL model checker for Henshin state spaces.
+ * @author Christian Krause
+ */
+public class OCLModelChecker implements ModelChecker {
+
+	public boolean checkFormula(String formula) throws ParseException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void parseFormula(String formula) throws ParseException {
+		// TODO Auto-generated method stub
+	}
+
+	public void setStateSpace(StateSpace stateSpace) {
+		// TODO Auto-generated method stub
+	}
+
+}
