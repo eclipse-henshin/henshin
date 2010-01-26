4b1497695ab2aaa1ac6df06abe1979b704206c4e
sjurack <sjurack@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2010-01-26 21:15:57 +0000

    minor improvements

--- plugins/org.eclipse.emf.henshin.diagram/src/org/eclipse/emf/henshin/diagram/actions/Action.java
+++ plugins/org.eclipse.emf.henshin.diagram/src/org/eclipse/emf/henshin/diagram/actions/Action.java
@@ -1,96 +1,137 @@
 package org.eclipse.emf.henshin.diagram.actions;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 /**
- * Actions consist of an {@link ActionType} and an number of
- * arguments, which are implemented as a simple {@link String} array.
- * Actions can be printed, parsed and compared using equals().
+ * This class represents an Action of a graph element. Actions consist of an
+ * {@link ActionType} and a number of string arguments. Actions can be printed,
+ * parsed and compared using equals().
+ * 
  * @generated NOT
  * @author Christian Krause
+ * @author Stefan Jurack
  */
 public class Action {
-	
-	// Action type.
-	private ActionType type;
-	
-	// Optional arguments.
-	private String[] arguments;
-	
+
 	/**
-	 * Default constructor.
-	 * @param type Action type.
-	 * @param arguments Optional arguments.
+	 * Separator of the action type information
 	 */
-	public Action(ActionType type, String... arguments) {
-		this.type = type;
-		this.arguments = arguments;
-	}
-	
-	public ActionType getType() {
-		return type;
-	}
-	
-	public String[] getArguments() {
-		return arguments;
-	}
-	
+	private static final String SEPARATOR_TYPE = ":";
+
 	/**
-	 * Parse an element action.
-	 * @param value String representation of the action.
+	 * Separator of action type arguments
+	 */
+	private static final String SEPARATOR_ARGS = ",";
+
+	private static final Pattern PATTERN_TYPE = Pattern.compile(SEPARATOR_TYPE);
+	private static final Pattern PATTERN_ARGS = Pattern.compile(SEPARATOR_ARGS);
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+	/**
+	 * Parses an action string for graph elements. Such string may have the form ".*:[.*[,.*]*]?".
+	 * 
+	 * @param value
+	 *            String representation of the action.
 	 * @return The parsed element action.
-	 * @throws ParseException On parse errors.
+	 * @throws ParseException
+	 *             On parse errors.
 	 */
 	public static Action parse(String value) throws ParseException {
-		
-		// Check if there is a colon:
-		int colon = value.indexOf(':');
-		ActionType type = ActionType.parse(colon < 0 ? value : value.substring(0,colon));
-		
-		// Parse the arguments:
-		List<String> arguments = new ArrayList<String>();
-		if (colon>=0) {
-			String args = value.substring(colon);
-			while (args!=null) {
-				int comma = args.indexOf(',');
-				String current;
-				if (comma>=0) {
-					current = args.substring(0,comma);
-					args = args.substring(comma);
-				} else {
-					current = args;
-					args = null;
-				}
-				current = current.trim();
-				if (current.length()!=0) {
-					arguments.add(current);
-				}
-			}
-		}
-		
+		/*
+		 * TODO A specification of the action elements syntax need to be
+		 * created. In particular, the input syntax (what the user inserts)
+		 * should be equal to displayed syntax. Or at least, the displayed
+		 * syntax should be validly used as input as well as a shorten form.<br>
+		 * Additionally, a default action type might be reasonable e.g.
+		 * ActionType.NONE.
+		 */
+
+		/*
+		 * Check for a colon, as the string before a colon is suggested to be an
+		 * action type informations.
+		 */
+		String[] aTypeAndArgs = PATTERN_TYPE.split(value, 2);
+		ActionType type = ActionType.parse(aTypeAndArgs[0]);
+
+		/*
+		 * Check for further arguments, which occur after the colon and which
+		 * have to be separated by a comma.
+		 */
+		String[] aArgs;
+		if (aTypeAndArgs.length == 2) {
+			aArgs = PATTERN_ARGS.split(aTypeAndArgs[1]);
+		} else {
+			aArgs = EMPTY_STRING_ARRAY;
+		}// if else
+
 		// Create and return the new action:
-		return new Action(type, arguments.toArray(new String[0]));
-	}
-	
+		return new Action(type, aArgs);
+
+	}// parse
+
+	// Action type.
+	private ActionType type;
+
+	// Optional arguments.
+	private String[] arguments;
+
+	/**
+	 * Default constructor.
+	 * 
+	 * @param type
+	 *            Action type. Must not be <code>null</code>!
+	 * @param arguments
+	 *            Optional arguments.
+	 */
+	public Action(ActionType type, String... arguments) {
+		if (type == null)
+			throw new IllegalArgumentException(
+					"Parameter type must not be null.");
+		this.type = type;
+		this.arguments = (arguments == null) ? EMPTY_STRING_ARRAY : arguments;
+	}// constructor
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
 	public boolean equals(Object object) {
-		if (object==this) return true;
+		if (object == this)
+			return true;
 		if (object instanceof Action) {
 			Action action = (Action) object;
-			return (type==action.getType() && Arrays.equals(arguments, action.getArguments()));
+			return (type == action.getType() && Arrays.equals(arguments, action
+					.getArguments()));
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Returns arguments this Action contains. If no arguments are specified,
+	 * this method returns an empty string array.
+	 * 
+	 * @return
+	 */
+	public String[] getArguments() {
+		return arguments;
+	}
+
+	/**
+	 * Returns the action type represented by this Action.
+	 * 
+	 * @return
+	 */
+	public ActionType getType() {
+		return type;
+	}
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#hashCode()
 	 */
 	@Override
@@ -101,18 +142,27 @@ public class Action {
 		}
 		return hash;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		String result = type.toString();
-		for (String argument : arguments) {
-			result = result + ',' + argument;
-		}
-		return result;
-	}
-	
-}
+
+		StringBuffer result = new StringBuffer();
+		result.append(type.toString());
+
+		if (arguments.length > 0) {
+			result.append(SEPARATOR_TYPE);
+			result.append(arguments[0]);
+			for (int i = 1; i < arguments.length; i++) {
+				result.append(SEPARATOR_ARGS);
+				result.append(arguments[i]);
+			}// for
+		}// if
+		return result.toString();
+	}// toString
+
+}// class
