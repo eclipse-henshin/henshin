8f31b12d2b32ad535ab7778e08fa1068bd21672f
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2010-04-22 08:10:11 +0000

    added dummy attribute action util class

---
+++ plugins/org.eclipse.emf.henshin.diagram/src/org/eclipse/emf/henshin/diagram/actions/AttributeActionUtil.java
@@ -0,0 +1,10 @@
+package org.eclipse.emf.henshin.diagram.actions;
+
+/**
+ * Static methods for determining and changing actions for attributes.
+ * @generated NOT
+ * @author Christian Krause
+ */
+public class AttributeActionUtil {
+
+}
