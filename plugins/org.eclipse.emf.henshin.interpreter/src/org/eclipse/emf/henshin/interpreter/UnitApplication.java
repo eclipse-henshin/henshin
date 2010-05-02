337f8875c4a698e12be1b3b7abc393aea9c752b9
sjurack <sjurack@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2010-05-02 11:53:16 +0000

    Unused import

--- plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/UnitApplication.java
+++ plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/UnitApplication.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 import java.util.Stack;
 
 import org.eclipse.emf.henshin.interpreter.interfaces.InterpreterEngine;
