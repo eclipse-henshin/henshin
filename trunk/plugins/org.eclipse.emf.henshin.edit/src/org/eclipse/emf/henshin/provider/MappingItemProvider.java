dc1e7f2ab23d9867ed16af188dead0f081cccd0f
sjurack <sjurack@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2010-12-10 14:54:19 +0000

    RESOLVED - bug 330377: Tree-based rule editor: Mapping not updated automatically when renaming Nodes https://bugs.eclipse.org/bugs/show_bug.cgi?id=330377

--- trunk/plugins/org.eclipse.emf.henshin.edit/src/org/eclipse/emf/henshin/provider/MappingItemProvider.java
+++ trunk/plugins/org.eclipse.emf.henshin.edit/src/org/eclipse/emf/henshin/provider/MappingItemProvider.java
@@ -11,6 +11,7 @@
  *******************************************************************************/
 package org.eclipse.emf.henshin.provider;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,7 +19,6 @@ import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.ResourceLocator;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -30,9 +30,15 @@ import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 import org.eclipse.emf.henshin.model.AmalgamationUnit;
+import org.eclipse.emf.henshin.model.BinaryFormula;
+import org.eclipse.emf.henshin.model.Formula;
+import org.eclipse.emf.henshin.model.Graph;
 import org.eclipse.emf.henshin.model.HenshinPackage;
 import org.eclipse.emf.henshin.model.Mapping;
+import org.eclipse.emf.henshin.model.NestedCondition;
 import org.eclipse.emf.henshin.model.Node;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.UnaryFormula;
 import org.eclipse.emf.henshin.provider.descriptors.MappingImagePropertyDescriptor;
 import org.eclipse.emf.henshin.provider.descriptors.MappingOriginPropertyDescriptor;
 
@@ -47,7 +53,7 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 		IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider,
 		IItemPropertySource {
 	
-	NodeMappingListener rmListener;
+	NodeListener nodeListener;
 	
 	/**
 	 * This constructs an instance from a factory and a notifier. <!--
@@ -58,7 +64,7 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 	public MappingItemProvider(AdapterFactory adapterFactory) {
 		super(adapterFactory);
 		
-		rmListener = new NodeMappingListener();
+		nodeListener = new NodeListener();
 	}
 	
 	/**
@@ -205,26 +211,52 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 	@Override
 	public void notifyChanged(Notification notification) {
 		
-		/*
-		 * Refresh the corresponding Nodes if the Mapping changes
-		 */
-		if (notification.getEventType() == Notification.SET) {
-			
-			Node n1 = (Node) notification.getNewValue();
-			Node n2 = (Node) notification.getOldValue();
-			
-			// refresh node labels
-			notifyNodeForRefresh(notification, n1);
-			notifyNodeForRefresh(notification, n2);
-			// refresh the mapping label itself
-			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(),
-					false, true));
-			
-		}// if
-		
+		if (notification.getNotifier() == this.getTarget()) {
+			/*
+			 * Refresh the corresponding Nodes if the Mapping changes
+			 */
+			if (notification.getEventType() == Notification.SET) {
+				
+				Node n1 = (Node) notification.getNewValue();
+				Node n2 = (Node) notification.getOldValue();
+				
+				// refresh node labels
+				notifyNodeForRefresh(notification, n1);
+				notifyNodeForRefresh(notification, n2);
+				// refresh the mapping label itself
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(),
+						false, true));
+				
+				removeNodeListener(n2);
+				addNodeListener(n1);
+				
+			}// if
+		}
 		updateChildren(notification);
 		super.notifyChanged(notification);
 	}
+	
+	/**
+	 * @param node
+	 */
+	private void addNodeListener(Node node) {
+		if (node != null) {
+			ItemProviderAdapter adapter = (ItemProviderAdapter) this.adapterFactory.adapt(node,
+					Node.class);
+			adapter.addListener(nodeListener);
+		}// if
+	}// addNodeListener
+	
+	/**
+	 * @param node
+	 */
+	private void removeNodeListener(Node node) {
+		if (node != null) {
+			ItemProviderAdapter adapter = (ItemProviderAdapter) this.adapterFactory.adapt(node,
+					Node.class);
+			adapter.removeListener(nodeListener);
+		}// if
+	}// removeNodeListener
 	
 	/**
 	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
@@ -258,16 +290,10 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 	@Override
 	public void unsetTarget(Notifier target) {
 		super.unsetTarget(target);
-		
-		// Get the container of the mapping and unregister the
-		// NodeMappingListener
-		EObject eobject = ((Mapping) target).eContainer();
-		if (eobject != null) {
-			ItemProviderAdapter eobjectAdapter = (ItemProviderAdapter) this.adapterFactory.adapt(
-					eobject, null);
-			eobjectAdapter.removeListener(rmListener);
-		}// if
-		
+		// remove node listeners, if mappings are no longer visible
+		Mapping mapping = (Mapping) target;
+		removeNodeListener(mapping.getImage());
+		removeNodeListener(mapping.getOrigin());
 	}// unsetTarget
 	
 	/*
@@ -279,19 +305,17 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 	@Override
 	public void setTarget(Notifier target) {
 		super.setTarget(target);
-		
-		// Get the container of the mapping and register the NodeMappingListener
-		EObject eobject = ((Mapping) target).eContainer();
-		if (eobject != null) {
-			ItemProviderAdapter eobjectAdapter = (ItemProviderAdapter) this.adapterFactory.adapt(
-					eobject, null);
-			eobjectAdapter.addListener(rmListener);
-		}// if
+		// add node listeners to fetch node renamings in order to update the
+		// mapping visualization
+		Mapping mapping = (Mapping) target;
+		addNodeListener(mapping.getImage());
+		addNodeListener(mapping.getOrigin());
 	}// setTarget
 	
 	/**
-	 * Notifies the given node to refresh its label (only). The given
-	 * notification is its base notification.
+	 * Notifies the given node to refresh its label (only). This affects the
+	 * icon in particular, which shows if the node is created, deleted or
+	 * preserve.
 	 * 
 	 * @param notification
 	 * @param node
@@ -306,31 +330,100 @@ public class MappingItemProvider extends ItemProviderAdapter implements IEditing
 	}// notifyNodeForRefresh
 	
 	/**
-	 * This Listener listens for added or removed Mapping object in order to
-	 * synchronize the visualisation (delete, create, preserve) of Nodes.
+	 * This Listener listens for events on nodes.
 	 * 
 	 * @author sjtuner
 	 * 
 	 */
-	private class NodeMappingListener implements INotifyChangedListener {
+	private class NodeListener implements INotifyChangedListener {
 		
 		@Override
 		public void notifyChanged(Notification notification) {
-			if (notification.getEventType() == Notification.ADD) {
-				if (notification.getNewValue() instanceof Mapping) {
-					Mapping map = (Mapping) notification.getNewValue();
-					MappingItemProvider.this.notifyNodeForRefresh(notification, map.getOrigin());
-					MappingItemProvider.this.notifyNodeForRefresh(notification, map.getImage());
-				}// if
-			} else if (notification.getEventType() == Notification.REMOVE) {
-				if (notification.getOldValue() instanceof Mapping) {
-					Mapping map = (Mapping) notification.getOldValue();
-					MappingItemProvider.this.notifyNodeForRefresh(notification, map.getOrigin());
-					MappingItemProvider.this.notifyNodeForRefresh(notification, map.getImage());
-				}// if
+			/*
+			 * Listen for Node renaming events.
+			 */
+			if (notification.getFeatureID(Node.class) == HenshinPackage.NODE__NAME) {
+				List<Mapping> mappings = findMappingsByNode((Node) notification.getNotifier());
+				
+				AdapterFactory fac = MappingItemProvider.this.adapterFactory;
+				for (Mapping m : mappings) {
+					// update the mapping visualization in the editor
+					Notification notif = new ViewerNotification(notification, m, false, true);
+					ItemProviderAdapter adapter = (ItemProviderAdapter) fac.adapt(m, Mapping.class);
+					adapter.fireNotifyChanged(notif);
+				}// for
+			}// if
+		}// notifyChanged
+		
+		/**
+		 * Finds all mappings related to the given node.
+		 * 
+		 * @param node
+		 * @return
+		 */
+		private List<Mapping> findMappingsByNode(Node node) {
+			List<Mapping> resultList = new ArrayList<Mapping>();
+			Graph graph = node.getGraph();
+			
+			if (graph.eContainer() instanceof Rule) {
+				Rule rule = (Rule) graph.eContainer();
+				collectMappingHelper(node, rule.getMappings(), resultList);
+			} else { // graph.eContainer() instance of NestedCondition
+				NestedCondition nc = (NestedCondition) graph.eContainer();
+				List<NestedCondition> allNestedConditions = new ArrayList<NestedCondition>();
+				collectNestedConditions(nc, allNestedConditions);
+				
+				for (NestedCondition n : allNestedConditions) {
+					collectMappingHelper(node, n.getMappings(), resultList);
+				}// for
+				
 			}// if else
 			
-		}// notifyChanged
+			return resultList;
+		}// findMappingsByNode
+		
+		/**
+		 * Iterates over the given mappingList and returns a resultList with all
+		 * mappings the given node is related to, whether as origin or image.
+		 * 
+		 * @param node
+		 * @param mappingList
+		 * @param resultList
+		 */
+		private void collectMappingHelper(Node node, List<Mapping> mappingList,
+				List<Mapping> resultList) {
+			for (Mapping m : mappingList) {
+				if ((m.getImage() == node) || (m.getOrigin() == node)) resultList.add(m);
+			}// for
+		}// collectMappingHelper
+		
+		/**
+		 * Collects recursively all {@link NestedCondition}s the given
+		 * <code>formula</code> is associated with, i.e. the hierarchy of the
+		 * given <code>formula</code>. If the formula is a
+		 * {@link NestedCondition} itself, it is added to the
+		 * <code>resultList</code>.<br>
+		 * Remark: Initially the resultList shall be not null. The given formula
+		 * is included in the list if it is a NestedCondition itself.
+		 * 
+		 * @param formula
+		 * @param resultList
+		 */
+		private void collectNestedConditions(Formula formula, List<NestedCondition> resultList) {
+			
+			if (formula == null) return;
+			
+			if (formula instanceof BinaryFormula) {
+				BinaryFormula bf = (BinaryFormula) formula;
+				collectNestedConditions(bf.getLeft(), resultList);
+				collectNestedConditions(bf.getRight(), resultList);
+			} else if (formula instanceof UnaryFormula) {
+				UnaryFormula uf = (UnaryFormula) formula;
+				collectNestedConditions(uf.getChild(), resultList);
+			} else { // it IS a nested condition
+				resultList.add((NestedCondition) formula);
+			}// if ifelse else
+		}// collectNestedCondition
 		
 	}// inner class
 	
