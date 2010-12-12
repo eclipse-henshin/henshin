6565de77c498d8a60a4e1bc34a602d78ff3310df
ebiermann <ebiermann@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2010-12-12 10:04:33 +0000

    fixed a bug with parameter passing for units

--- trunk/plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/UnitApplication.java
+++ trunk/plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/UnitApplication.java
@@ -1,13 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin, 
- * Philipps-University Marburg and others. All rights reserved. 
- * This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin,
+ * Philipps-University Marburg and others. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Technical University Berlin - initial API and implementation
+ * 
+ * Contributors: Technical University Berlin - initial API and implementation
  *******************************************************************************/
 package org.eclipse.emf.henshin.interpreter;
 
@@ -161,7 +159,8 @@ public class UnitApplication extends Observable {
 			Parameter sourceParameter = mapping.getSource();
 			Parameter targetParameter = mapping.getTarget();
 			
-			if (sourceParameter.getUnit() == childUnit.getTransformationUnit()) {
+			if (sourceParameter.getUnit() == childUnit.getTransformationUnit()
+					&& childUnit.parameterValues.containsKey(sourceParameter)) {
 				parameterValues
 						.put(targetParameter, childUnit.parameterValues.get(sourceParameter));
 			}
