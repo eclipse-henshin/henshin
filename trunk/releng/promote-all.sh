d0d6fd072d61775d185709380348c2b36251f1c0
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2011-02-11 19:45:55 +0000

    update promote settings

--- trunk/releng/promote-all.sh
+++ trunk/releng/promote-all.sh
@@ -4,22 +4,24 @@
 JAVA_HOME=/opt/public/common/jdk-1.6.x86_64
 ANT_HOME=/opt/public/common/apache-ant-1.7.1
 ANT=/opt/public/common/apache-ant-1.7.1/bin/ant
+
+# Directory for the drops:
 DROPS=/home/data/httpd/download.eclipse.org/modeling/emft/henshin/downloads/drops
 
-function promote {
+# Releng directories:
+NIGHTLY=/shared/jobs/cbi_henshin_nightly/workspace/build/org.eclipse.henshin.releng
+RELEASE=/shared/jobs/cbi_henshin_release/workspace/build/org.eclipse.henshin.releng
 
-	// run ant:
-	$ANT -f $1/promote.xml -Dpromote.properties=$1/promote.properties
-	
-	// clean up:
-	rm $DROPS/*/*/Henshin-examples* 2> /dev/null
-	rm $DROPS/*/*/Henshin-Update* 2> /dev/null
-	rm $DROPS/*/*/build.cfg 2> /dev/null
-	rm $DROPS/*/*/echoproperties* 2> /dev/null
-	rm $DROPS/*/*/directory.txt 2> /dev/null
-	rm -R $DROPS/*/*/compilelogs 2> /dev/null
-	
-}
 
-promote /shared/jobs/cbi_henshin_nightly/workspace/build/org.eclipse.henshin.releng
-promote /shared/jobs/cbi_henshin_release/workspace/build/org.eclipse.henshin.releng
+# Run the promote script:
+$ANT -f $NIGHTLY/promote.xml -Dpromote.properties=$NIGHTLY/promote.properties
+$ANT -f $RELEASE/promote.xml -Dpromote.properties=$RELEASE/promote.properties
+
+# Clean up:
+rm $DROPS/*/*/Henshin-examples* 2> /dev/null
+rm $DROPS/*/*/Henshin-Update* 2> /dev/null
+rm $DROPS/*/*/build.cfg 2> /dev/null
+rm $DROPS/*/*/echoproperties* 2> /dev/null
+rm $DROPS/*/*/directory.txt 2> /dev/null
+rm -R $DROPS/*/*/compilelogs 2> /dev/null
+
