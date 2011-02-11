04a70b50b5f307995cd7662056b25e9de9ea751a
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2011-02-11 14:47:35 +0000

    updated promote settings

--- trunk/releng/promote-all.sh
+++ trunk/releng/promote-all.sh
@@ -4,11 +4,23 @@
 JAVA_HOME=/opt/public/common/jdk-1.6.x86_64
 ANT_HOME=/opt/public/common/apache-ant-1.7.1
 ANT=/opt/public/common/apache-ant-1.7.1/bin/ant
+DROPS=/home/data/httpd/download.eclipse.org/modeling/emft/henshin/downloads/drops
 
-# Workspaces:
-NIGHTLY=/shared/jobs/cbi_henshin_nightly/workspace/build/org.eclipse.henshin.releng
-RELEASE=/shared/jobs/cbi_henshin_release/workspace/build/org.eclipse.henshin.releng
+function promote {
 
-# Run ant...
-$ANT -f $NIGHTLY/promote.xml -Dpromote.properties=$NIGHTLY/promote.properties
-#$ANT -f $RELEASE/promote.xml -Dpromote.properties=$RELEASE/promote.properties
+	// run ant:
+	$ANT -f $1/promote.xml -Dpromote.properties=$1/promote.properties
+	
+	// clean up:
+	rm $DROPS/*/*/Henshin-examples*
+	rm $DROPS/*/*/Henshin-Update*
+	rm $DROPS/*/*/build.cfg
+	rm $DROPS/*/*/echoproperties*
+	rm $DROPS/*/*/directory.txt
+	rm -R $DROPS/*/*/compilelogs
+	
+}
+
+promote /shared/jobs/cbi_henshin_nightly/workspace/build/org.eclipse.henshin.releng
+promote /shared/jobs/cbi_henshin_release/workspace/build/org.eclipse.henshin.releng
+
