f93ec5d03609f99070af713998031d5f6bb4e3a5
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2011-02-11 11:31:02 +0000

    updated promote settings

--- trunk/releng/promote.sh
+++ trunk/releng/promote.sh
@@ -6,9 +6,8 @@ ANT_HOME=/opt/public/common/apache-ant-1.7.1
 ANT=/opt/public/common/apache-ant-1.7.1/bin/ant
 
 # Project:
-#WORKSPACE=/shared/jobs/cbi_henshin_nightly/workspace
-#RELENG=$WORKSPACE/build/org.eclipse.henshin.releng
-RELENG=.
+WORKSPACE=/shared/jobs/cbi_henshin_nightly/workspace
+RELENG=$WORKSPACE/build/org.eclipse.henshin.releng
 
 # Run ant...
 $ANT -f $RELENG/promote.xml -Dpromote.properties=$RELENG/promote-N.properties
