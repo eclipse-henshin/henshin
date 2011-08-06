95a0e0a3642353184eeb38baca2f89a2e47f603c
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2011-08-06 10:49:12 +0000

    removed derived templates

--- trunk/plugins/org.eclipse.emf.henshin.codegen.model/src/org/eclipse/emf/henshin/codegen/model/util/GenHenshinSwitch.java
+++ trunk/plugins/org.eclipse.emf.henshin.codegen.model/src/org/eclipse/emf/henshin/codegen/model/util/GenHenshinSwitch.java
@@ -6,9 +6,10 @@
  */
 package org.eclipse.emf.henshin.codegen.model.util;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.Switch;
 import org.eclipse.emf.henshin.codegen.model.*;
 
 /**
@@ -24,7 +25,7 @@ import org.eclipse.emf.henshin.codegen.model.*;
  * @see org.eclipse.emf.henshin.codegen.model.GenHenshinPackage
  * @generated
  */
-public class GenHenshinSwitch<T> extends Switch<T> {
+public class GenHenshinSwitch<T> {
 	/**
 	 * The cached model package
 	 * <!-- begin-user-doc -->
@@ -46,16 +47,14 @@ public class GenHenshinSwitch<T> extends Switch<T> {
 	}
 
 	/**
-	 * Checks whether this is a switch for the given package.
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @parameter ePackage the package in question.
-	 * @return whether this is a switch for the given package.
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
-	protected boolean isSwitchFor(EPackage ePackage) {
-		return ePackage == modelPackage;
+	public T doSwitch(EObject theEObject) {
+		return doSwitch(theEObject.eClass(), theEObject);
 	}
 
 	/**
@@ -65,7 +64,26 @@ public class GenHenshinSwitch<T> extends Switch<T> {
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
+	protected T doSwitch(EClass theEClass, EObject theEObject) {
+		if (theEClass.eContainer() == modelPackage) {
+			return doSwitch(theEClass.getClassifierID(), theEObject);
+		}
+		else {
+			List<EClass> eSuperTypes = theEClass.getESuperTypes();
+			return
+				eSuperTypes.isEmpty() ?
+					defaultCase(theEObject) :
+					doSwitch(eSuperTypes.get(0), theEObject);
+		}
+	}
+
+	/**
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
+	 * @generated
+	 */
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch (classifierID) {
 			case GenHenshinPackage.GEN_HENSHIN: {
@@ -168,7 +186,6 @@ public class GenHenshinSwitch<T> extends Switch<T> {
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
-	@Override
 	public T defaultCase(EObject object) {
 		return null;
 	}
