7e5f8fbfb060b5dc6fa3d6ccc63df2a7a62df5d1
frieger <frieger@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2011-11-30 10:41:06 +0000

    Branch for developing the 2010 to 2011 model transformation

---
+++ branches/2010to2011transformation/org.eclipse.emf.henshin.statespace.explorer/src/org/eclipse/emf/henshin/statespace/explorer/commands/SetRulesCommand.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin, 
+ * Philipps-University Marburg and others. All rights reserved. 
+ * This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     CWI Amsterdam - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.henshin.statespace.explorer.commands;
+
+import java.util.List;
+
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.statespace.StateSpace;
+import org.eclipse.emf.henshin.statespace.StateSpaceException;
+import org.eclipse.emf.henshin.statespace.StateSpaceManager;
+import org.eclipse.emf.henshin.statespace.StateSpacePlugin;
+
+/**
+ * @author Christian Krause
+ */
+public class SetRulesCommand extends AbstractStateSpaceCommand {
+	
+	// Rules to be used:
+	private List<Rule> rules;
+
+	/**
+	 * Default constructor.
+	 * @param manager Statespace manager.
+	 */
+	public SetRulesCommand(StateSpaceManager manager, List<Rule> rules) {
+		super("set rules", manager);
+		this.rules = rules;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.emf.henshin.statespace.explorer.commands.AbstractStateSpaceCommand#doExecute()
+	 */
+	@Override
+	public void doExecute() throws StateSpaceException {
+		
+		StateSpace stateSpace = getStateSpaceManager().getStateSpace();
+		stateSpace.getRules().clear();
+		stateSpace.getRules().addAll(rules);
+		
+		// Initialize properties for the new rules.
+		StateSpacePlugin.INSTANCE.getPropertiesManager().initialize(stateSpace);	
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.gef.commands.Command#canUndo()
+	 */
+	@Override
+	public boolean canUndo() {
+		return false; // command taints the state space.
+	}
+
+}
