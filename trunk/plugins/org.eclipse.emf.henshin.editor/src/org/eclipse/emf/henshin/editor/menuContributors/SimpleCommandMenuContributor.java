ddc26b200af3f991a81caaa862024482f1101568
sjurack <sjurack@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2012-02-29 16:19:31 +0000

    Added an action command to Rules which supports for deduction (and consequently the creation) of parameters according to object names and attribute values. This is a pretty simplistic approach which is, nevertheless, meaningful in most cases.

--- trunk/plugins/org.eclipse.emf.henshin.editor/src/org/eclipse/emf/henshin/editor/menuContributors/SimpleCommandMenuContributor.java
+++ trunk/plugins/org.eclipse.emf.henshin.editor/src/org/eclipse/emf/henshin/editor/menuContributors/SimpleCommandMenuContributor.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin, 
- * Philipps-University Marburg and others. All rights reserved. 
- * This program and the accompanying materials are made 
+ * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin,
+ * Philipps-University Marburg and others. All rights reserved.
+ * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -15,6 +15,7 @@ import java.util.List;
 
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.henshin.editor.commands.CreateMappedNodeCommand;
+import org.eclipse.emf.henshin.editor.commands.DeduceParametersCommand;
 import org.eclipse.emf.henshin.editor.commands.MenuContributor;
 import org.eclipse.emf.henshin.model.Rule;
 import org.eclipse.jface.action.IMenuManager;
@@ -32,6 +33,7 @@ public class SimpleCommandMenuContributor extends MenuContributor {
 	public static MenuContributor INSTANCE = new SimpleCommandMenuContributor();
 	
 	private static final String COMMAND_LABEL_MAPPED_NODE = "CreateMappedNode";
+	private static final String COMMAND_LABEL_DEDUCE_PARAMETERS = "DeduceParameters";
 	
 	@Override
 	public void contributeActions(IMenuManager menuManager, List<?> selection) {
@@ -52,6 +54,8 @@ public class SimpleCommandMenuContributor extends MenuContributor {
 		if ((selection.size() == 1) && (selection.get(0) instanceof Rule)) {
 			menuManager.add(createAction(getLabel(COMMAND_LABEL_MAPPED_NODE),
 					new CreateMappedNodeCommand((Rule) selection.get(0))));
+			menuManager.add(createAction(getLabel(COMMAND_LABEL_DEDUCE_PARAMETERS),
+					new DeduceParametersCommand((Rule) selection.get(0))));
 		}
 	}
 	
