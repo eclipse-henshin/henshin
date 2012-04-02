b13d202347b5b60d35cb416492663b79146bd852
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2012-04-02 20:37:18 +0000

    Tag for 0.9.0

---
+++ tags/0.9.0/org.eclipse.emf.henshin.editor/src/org/eclipse/emf/henshin/presentation/HenshinIcons.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin, 
+ * Philipps-University Marburg and others. All rights reserved. 
+ * This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     CWI Amsterdam - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.henshin.presentation;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * Henshin and Ecore icons.
+ * 
+ * @author Christian Krause
+ */
+public class HenshinIcons {
+	
+	// Ecore edit plugin id:
+	private static final String ECORE_EDIT = "org.eclipse.emf.ecore.edit";
+	
+	// Henshin edit plugin id:
+	private static final String HENSHIN_EDIT = "org.eclipse.emf.henshin.edit";
+	
+	// Ecore icons:
+	public static final Image EPACKAGE = load(ECORE_EDIT, "icons/full/obj16/EPackage.gif");
+	public static final Image ECLASS = load(ECORE_EDIT, "icons/full/obj16/EClass.gif");
+	public static final Image EREFERENCE = load(ECORE_EDIT, "icons/full/obj16/EReference.gif");
+	
+	// Henshin icons:
+	public static final Image RULE = load(HENSHIN_EDIT, "icons/full/obj16/Rule.gif");
+	public static final Image NOE = load(HENSHIN_EDIT, "icons/full/obj16/Node.gif");
+	public static final Image EDGE = load(HENSHIN_EDIT, "icons/full/obj16/Edge.gif");
+	
+	/*
+	 * Load an image.
+	 */
+	private static Image load(String pluginId, String path) {
+		ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin(pluginId, path);
+		return (descriptor != null) ? descriptor.createImage() : null;
+	}
+	
+}
