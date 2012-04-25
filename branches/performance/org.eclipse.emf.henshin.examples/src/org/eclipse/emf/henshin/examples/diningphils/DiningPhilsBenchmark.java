1f88661392809075f0df3038b1eaa3ba2dc70189
ckrause <ckrause@9e524e4a-a177-0410-acd0-3f3181ad8b4e>
2012-04-25 19:16:17 +0000

    branch for performance optimizations

---
+++ branches/performance/org.eclipse.emf.henshin.examples/src/org/eclipse/emf/henshin/examples/diningphils/DiningPhilsBenchmark.java
@@ -0,0 +1,103 @@
+package org.eclipse.emf.henshin.examples.diningphils;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.henshin.interpreter.EmfEngine;
+import org.eclipse.emf.henshin.interpreter.RuleApplication;
+import org.eclipse.emf.henshin.matching.EmfGraph;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.statespace.StateSpace;
+import org.eclipse.emf.henshin.statespace.StateSpaceException;
+import org.eclipse.emf.henshin.statespace.StateSpaceFactory;
+import org.eclipse.emf.henshin.statespace.StateSpaceManager;
+import org.eclipse.emf.henshin.statespace.StateSpaceProperties;
+import org.eclipse.emf.henshin.statespace.resource.StateSpaceResourceSet;
+import org.eclipse.emf.henshin.statespace.util.StateSpaceExplorationHelper;
+
+public class DiningPhilsBenchmark {
+
+	public static void doBenchmark(int maxPhils, int numThreads) {
+
+		System.out.println("Starting benchmark...");
+		System.out.println("NumThreads: " + numThreads);
+		System.out.println("MaxMemory: " + Runtime.getRuntime().maxMemory() / (1024 * 1024) + "MB\n");
+
+		// Create a resource set with a base directory:
+		StateSpaceResourceSet resourceSet = new StateSpaceResourceSet(
+				"src/org/eclipse/emf/henshin/examples/diningphils/model");
+		
+		// Load the state space and create a state space manager:
+		StateSpace stateSpace = resourceSet.getStateSpace("3-phils.statespace");
+		StateSpaceManager manager = StateSpaceFactory.eINSTANCE.createStateSpaceManager(stateSpace, numThreads);
+		
+		// To improve the performance, we omit the identity types:
+		stateSpace.getProperties().remove(StateSpaceProperties.IDENTITY_TYPES);
+		
+		// Find the rule for adding a philosopher:
+		Rule createPhilRule = stateSpace.getRules().get(0).
+				getTransformationSystem().findRuleByName("createPhil");
+
+		// Now do the benchmark...
+		try {
+			System.out.println("Phils\tStates\tTrans\tTime");
+
+			for (int phils=3; phils<=maxPhils; phils++) {
+				
+				// First reset the state space:
+				manager.resetStateSpace();
+				for (int i=0; i<10; i++) {
+					System.gc();
+				}
+				
+				// Then explore it again:
+				StateSpaceExplorationHelper helper = new StateSpaceExplorationHelper(manager);
+				long time = System.currentTimeMillis();
+				helper.doExploration(-1, new NullProgressMonitor());
+				time = (System.currentTimeMillis() - time);
+
+				int expectedStates = (int) Math.pow(3, phils);
+				if (stateSpace.getStateCount()!=expectedStates || !stateSpace.getOpenStates().isEmpty()) {
+					throw new StateSpaceException("Unexpected number of states: " +
+							stateSpace.getStateCount() + "(" +
+							stateSpace.getOpenStates().size() + ")");
+				}
+				
+				System.out.println(phils + "\t" + 
+								stateSpace.getStateCount() + "\t" + 
+								stateSpace.getTransitionCount() + "\t" + 
+								time);
+				
+				// Add a philosopher:
+				EmfGraph initialStateGraph = manager.getModel(stateSpace.getInitialStates().get(0)).getEmfGraph();
+				EmfEngine engine = new EmfEngine(initialStateGraph);
+				RuleApplication app = new RuleApplication(engine, createPhilRule);
+				app.apply();
+				
+			}	
+		}
+		catch (StateSpaceException e) {
+			e.printStackTrace();
+		}
+		finally {
+			manager.shutdown();
+			manager = null;
+			for (int i=0; i<10; i++) {
+				System.gc();
+			}
+		}
+		System.out.println();
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int threads = Runtime.getRuntime().availableProcessors();
+		
+		System.out.println("\n******* WARMUP PHASE ********\n");
+		doBenchmark(8, threads);
+		
+		System.out.println("\n******* BENCHMARK ********\n");
+		doBenchmark(13, threads);
+		
+	}
+
+}
---
+++ branches/performance/org.eclipse.emf.henshin.examples/src/org/eclipse/emf/henshin/examples/sierpinski/SierpinskiBenchmark.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2010 CWI Amsterdam, Technical University Berlin, 
+ * Philipps-University Marburg and others. All rights reserved. 
+ * This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Technical University Berlin - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.henshin.examples.sierpinski;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.henshin.interpreter.EmfEngine;
+import org.eclipse.emf.henshin.interpreter.RuleApplication;
+import org.eclipse.emf.henshin.interpreter.util.Match;
+import org.eclipse.emf.henshin.matching.EmfGraph;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.TransformationSystem;
+import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
+
+/**
+ * A benchmark constructing multiple levels of a Sierpinski triangle.
+ * 
+ * @see <a href="http://en.wikipedia.org/wiki/Sierpinski_triangle">Sierpinski Triangle</a>
+ */
+public class SierpinskiBenchmark {
+	
+	public static void main(String[] args) {
+
+		// Create a resource set with a base directory:
+		HenshinResourceSet resourceSet = new HenshinResourceSet(
+				"src/org/eclipse/emf/henshin/examples/sierpinski/model");
+		
+		// Load the transformation system:
+		TransformationSystem trasys = resourceSet.getTransformationSystem("sierpinski.henshin");
+
+		// Load the first level of the Sierpinski triangle:
+		EObject container = resourceSet.getFirstRoot("sierpinski-start.xmi");
+		
+		// Initialize the Henshin interpreter:
+		EmfGraph graph = new EmfGraph(container);
+		graph.removeEObject(container);
+		EmfEngine engine = new EmfEngine(graph);
+
+		// Load the rule:
+		Rule addTriangleRule = trasys.findRuleByName("AddTriangle");
+
+		System.out.println(Runtime.getRuntime().maxMemory() / (1024 * 1024) + "MB available memory\n");
+		
+		// Iteratively compute the Sierpinski triangle:
+		int i = 1;
+		while (true) {
+			
+			// Find all matches:
+			long startTime = System.nanoTime();
+			RuleApplication addTriangle = new RuleApplication(engine,
+					addTriangleRule);
+			List<Match> matches = addTriangle.findAllMatches();
+			long matchingTime = (System.nanoTime() - startTime) / 1000000;
+			
+			System.out.println("Level: " + i);
+			System.out.println("Rule applications:" + matches.size());
+			System.out.println("Matching: " + matchingTime + "ms");
+
+			// Apply rule with all matches:
+			startTime = System.nanoTime();
+			for (Match match : matches) {
+				addTriangle = new RuleApplication(engine, addTriangleRule);
+				addTriangle.setMatch(match);
+				addTriangle.apply();
+			}
+			long runtime = (System.nanoTime() - startTime) / 1000000;
+
+			System.out.println("Application: " + runtime + "ms");
+			System.out.println("Total: " + (matchingTime + runtime) + "ms");
+			System.out.println("Nodes: " + graph.geteObjects().size());
+			System.out.println();
+			i++;
+			
+		}
+		
+	}
+	
+}
