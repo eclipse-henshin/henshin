f14d119616e0737a8c71e64eadfee40bf855ce11
Christian Krause <henshin.ck@gmail.com>
2014-07-09 20:48:00 +0200

    Fixed color mode bug

--- plugins/org.eclipse.emf.henshin.edit/src/org/eclipse/emf/henshin/provider/util/HenshinColorMode.java
+++ plugins/org.eclipse.emf.henshin.edit/src/org/eclipse/emf/henshin/provider/util/HenshinColorMode.java
@@ -11,6 +11,7 @@ package org.eclipse.emf.henshin.provider.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -24,56 +25,59 @@ import org.eclipse.emf.henshin.model.Action;
 import org.osgi.framework.Bundle;
 
 /**
- * Color modes for Henshin models and diagrams. It has no dependencies
- * to UI plug-ins, but to the platform. Maybe this dependency can be
- * removed in the future.
+ * Color modes for Henshin models and diagrams. It has no dependencies to UI
+ * plug-ins, but to the platform. Maybe this dependency can be removed in the
+ * future.
  * 
  * @author Christian Krause
  *
  */
 public class HenshinColorMode {
-	
+
 	public static class Color {
-		
+
 		public final int red, green, blue;
-		
+
 		public Color(int red, int green, int blue) {
 			this.red = red;
 			this.green = green;
 			this.blue = blue;
 		}
-		
+
 		@Override
 		public int hashCode() {
 			return red + green + blue; // improve this..
 		}
-		
+
 		@Override
 		public boolean equals(Object o) {
 			if (o instanceof Color) {
 				Color c = (Color) o;
-				return c.red==red && c.green==green && c.blue==blue;
+				return c.red == red && c.green == green && c.blue == blue;
 			}
 			return false;
 		}
-		
+
 		public URI toURI() {
-			return URI.createURI("color://rgb/" + red + "/" + green + "/" + blue);
+			return URI.createURI("color://rgb/" + red + "/" + green + "/"
+					+ blue);
 		}
-		
+
 	}
-	
-	public static final Map<String,HenshinColorMode> REGISTRY;
+
+	public static final Map<String, HenshinColorMode> REGISTRY;
 
 	private static HenshinColorMode DEFAULT;
 
 	static {
-		REGISTRY = new LinkedHashMap<String,HenshinColorMode>() {
+		REGISTRY = new LinkedHashMap<String, HenshinColorMode>() {
 			private static final long serialVersionUID = 1L;
+
 			@Override
 			public HenshinColorMode get(Object v) {
 				return super.get(String.valueOf(v).toLowerCase());
 			}
+
 			@Override
 			public HenshinColorMode put(String v, HenshinColorMode mode) {
 				return super.put(String.valueOf(v).toLowerCase(), mode);
@@ -83,74 +87,76 @@ public class HenshinColorMode {
 		URL colorModesURL = bundle.getEntry("colorModes");
 		try {
 			File dir;
- 			try {
+			try {
 				dir = new File(FileLocator.resolve(colorModesURL).toURI());
-			} catch(URISyntaxException e) {
+			} catch (URISyntaxException e) {
 				dir = new File(FileLocator.resolve(colorModesURL).getPath());
 			}
-		    for (File file : dir.listFiles()) {
-		    	if (file.getAbsolutePath().endsWith(".properties")) {
-		    		Properties properties = new Properties();
-		    		properties.load(new FileInputStream(file));
-		    		HenshinColorMode mode = new HenshinColorMode(properties);
-		    		REGISTRY.put(mode.getName(), mode);
-		    	}
-		    }
+			for (File file : dir.listFiles()) {
+				if (file.getAbsolutePath().endsWith(".properties")) {
+					Properties properties = new Properties();
+					properties.load(new FileInputStream(file));
+					HenshinColorMode mode = new HenshinColorMode(properties);
+					REGISTRY.put(mode.getName(), mode);
+				}
+			}
 		} catch (Throwable t) {
 			t.printStackTrace();
-		}		
+		}
 	}
-	
-	public static final String BG_RULE = "BG_RULE"; 
+
+	public static final String BG_RULE = "BG_RULE";
 	public static final String BG_UNIT = "BG_UNIT";
 	public static final String BG_INVOCATION = "BG_INVOCATION";
 
-	public static final String BG_ACTION_PRESERVE = "BG_ACTION_PRESERVE"; 
+	public static final String BG_ACTION_PRESERVE = "BG_ACTION_PRESERVE";
 	public static final String BG_ACTION_CREATE = "BG_ACTION_CREATE";
 	public static final String BG_ACTION_DELETE = "BG_ACTION_DELETE";
 	public static final String BG_ACTION_FORBID = "BG_ACTION_FORBID";
 	public static final String BG_ACTION_REQUIRE = "BG_ACTION_REQUIRE";
 
-	public static final String FG_ACTION_PRESERVE = "FG_ACTION_PRESERVE"; 
+	public static final String FG_ACTION_PRESERVE = "FG_ACTION_PRESERVE";
 	public static final String FG_ACTION_CREATE = "FG_ACTION_CREATE";
 	public static final String FG_ACTION_DELETE = "FG_ACTION_DELETE";
 	public static final String FG_ACTION_FORBID = "FG_ACTION_FORBID";
 	public static final String FG_ACTION_REQUIRE = "FG_ACTION_REQUIRE";
 
 	/**
-	 * Get the default color mode. Can return <code>null</code>, but it shouldn't.
+	 * Get the default color mode. Can return <code>null</code>, but it
+	 * shouldn't.
+	 * 
 	 * @return The default color mode.
 	 */
 	public static HenshinColorMode getDefaultColorMode() {
-		if (DEFAULT==null) {
+		if (DEFAULT == null) {
 			DEFAULT = REGISTRY.get("default");
-			if (DEFAULT==null && !HenshinColorMode.REGISTRY.isEmpty()) {
+			if (DEFAULT == null && !HenshinColorMode.REGISTRY.isEmpty()) {
 				DEFAULT = HenshinColorMode.REGISTRY.get(0);
 			}
 		}
 		return DEFAULT;
 	}
-	
+
 	private Properties properties;
 
-	private Map<String,Color> colors;
-	
+	private Map<String, Color> colors;
+
 	public HenshinColorMode(Properties properties) {
 		this.properties = properties;
-		this.colors = new HashMap<String,Color>();
+		this.colors = new HashMap<String, Color>();
 	}
 
 	public Color getColor(String type) {
 		Color color = colors.get(type);
-		if (color!=null) {
+		if (color != null) {
 			return color;
 		}
 		String value = properties.getProperty(type);
-		if (value==null) {
+		if (value == null) {
 			return null;
 		}
 		String[] vals = value.split(",");
-		if (vals.length!=3) {
+		if (vals.length != 3) {
 			return null;
 		}
 		try {
@@ -164,23 +170,24 @@ public class HenshinColorMode {
 			return null;
 		}
 	}
-	
+
 	public Color getActionColor(Action action, boolean foreground) {
-		if (action==null) {
+		if (action == null) {
 			return null;
 		}
 		return getColor(getActionColorKey(action, foreground));
 	}
 
 	public static String getActionColorKey(Action action, boolean foreground) {
-		if (action==null) {
+		if (action == null) {
 			return null;
 		}
-		return (foreground ? "FG_" : "BG_") + "ACTION_" + action.getType().toString().toUpperCase();
+		return (foreground ? "FG_" : "BG_") + "ACTION_"
+				+ action.getType().toString().toUpperCase();
 	}
-	
+
 	public String getName() {
 		return properties.getProperty("colorModeName");
 	}
-	
+
 }
