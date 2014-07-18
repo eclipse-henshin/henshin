edeab03cb571fad7968c235e4ec765562bb47611
Christian Krause <henshin.ck@gmail.com>
2014-07-18 21:22:24 +0200

    added proxy setting to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-ls /shared
-ls /shared/common
-ls /shared/common/apache-maven-3.0-beta-1
-
 export PATH=$PATH:/shared/common/apache-maven-3.0-beta-1/bin
+
+cat << EOF > $HOME/.m2/settings.xml
+<settings>
+  <proxies>
+   <proxy>
+      <active>true</active>
+      <protocol>http</protocol>
+      <host>proxy.eclipse.org</host>
+      <port>9898</port>
+      <nonProxyHosts>eclipse.org</nonProxyHosts>
+    </proxy>
+  </proxies>
+</settings>
+EOF
+
 mvn clean install
