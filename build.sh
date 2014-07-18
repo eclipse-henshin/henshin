5f32b9ef43a24b7ff0d2ff11845c87fb420ba7ba
Christian Krause <henshin.ck@gmail.com>
2014-07-18 21:14:39 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget http://ftp.halifax.rwth-aachen.de/apache/maven/maven-3/3.2.2/binaries/apache-maven-3.2.2-bin.tar.gz
-tar xvfz apache-maven-3.2.2-bin.tar.gz
-export PATH=$PATH:$PWD/apache-maven-3.2.2/bin
+ls /shared
+ls /shared/common
+ls /shared/common/apache-maven-3.0-beta-1
+
+export PATH=$PATH:/shared/common/apache-maven-3.0-beta-1/bin
 mvn clean install
