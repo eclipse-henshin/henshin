8553a6d1a37f7e7775d0cbef7e3bd1ba55812303
Christian Krause <henshin.ck@gmail.com>
2014-07-30 21:48:43 +0200

    updated promote script

--- promote.sh
+++ promote.sh
@@ -7,7 +7,7 @@ fi
 
 BUILD=$1
 
-SRC=/opt/public/jobs/cbi_henshin_$BUILD/workspace/henshin/p2updatesite/target/repository
+SRC=/opt/public/jobs/cbi_henshin_$BUILD/workspace/p2updatesite/target/repository
 TRG=/home/data/httpd/download.eclipse.org/modeling/emft/henshin/updates/$BUILD
 
 rm -R $TRG/* 2> /dev/null
