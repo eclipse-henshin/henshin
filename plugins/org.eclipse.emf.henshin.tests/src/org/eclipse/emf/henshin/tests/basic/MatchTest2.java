e6edbf0217a083608ad38a9bcb5612bda5b676bd
Christian krause <henshin.ck@gmail.com>
2014-12-25 13:14:49 +0100

    Fix for Bug 453287
    Change-Id: I10b8074ef43326d2c12ad1f0dea8a626da7f2771
    Signed-off-by: Christian krause <henshin.ck@gmail.com>

---
+++ plugins/org.eclipse.emf.henshin.tests/src/org/eclipse/emf/henshin/tests/basic/MatchTest2.java
@@ -0,0 +1,46 @@
+package org.eclipse.emf.henshin.tests.basic;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.henshin.interpreter.EGraph;
+import org.eclipse.emf.henshin.interpreter.Engine;
+import org.eclipse.emf.henshin.interpreter.Match;
+import org.eclipse.emf.henshin.interpreter.impl.EGraphImpl;
+import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
+import org.eclipse.emf.henshin.interpreter.util.InterpreterUtil;
+import org.eclipse.emf.henshin.model.Action;
+import org.eclipse.emf.henshin.model.HenshinFactory;
+import org.eclipse.emf.henshin.model.Node;
+import org.eclipse.emf.henshin.model.Rule;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MatchTest2 {
+
+	@Test
+	public void bug453287() {
+
+		Rule rule = HenshinFactory.eINSTANCE.createRule();
+		Node t = rule.createNode(EcorePackage.eINSTANCE.getEDataType());
+		Node a1 = rule.createNode(EcorePackage.eINSTANCE.getEAttribute());
+		Node a2 = rule.createNode(EcorePackage.eINSTANCE.getEAttribute());
+		rule.createEdge(a1, t, EcorePackage.eINSTANCE.getEAttribute_EAttributeType());
+		rule.createEdge(a2, t, EcorePackage.eINSTANCE.getEAttribute_EAttributeType());
+
+		t.setAction(new Action(Action.Type.REQUIRE));
+		Assert.assertEquals(2, rule.getLhs().getNodes().size());
+		Assert.assertEquals(0, rule.getLhs().getEdges().size());
+
+		EGraph graph = new EGraphImpl();
+		graph.add(EcoreFactory.eINSTANCE.createEAttribute());
+		graph.add(EcoreFactory.eINSTANCE.createEAttribute());
+
+		Engine engine = new EngineImpl();
+		List<Match> matches = InterpreterUtil.findAllMatches(engine, rule, graph, null);
+		Assert.assertEquals(0, matches.size());
+
+	}
+
+}
