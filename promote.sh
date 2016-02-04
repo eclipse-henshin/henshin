707aff478e2bc16b2db8732c66070c84d6ae3bf7
Christian Krause <henshin.ck@gmail.com>
2016-02-04 18:08:14 +0100

    Fixed promote.sh script
    Change-Id: I61e05ad3b207ede2ec9b1408ec1aa42456cf7077

--- promote.sh
+++ promote.sh
@@ -5,10 +5,15 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [ ! -d "p2updatesite" ]; then
+    echo "Missing directory: p2updatesite"
+    exit 2
+fi
+
 BUILD=$1
 
-SRC=/opt/public/jobs/cbi_henshin_$BUILD/workspace/p2updatesite/target/repository
-TRG=/home/data/httpd/download.eclipse.org/modeling/emft/henshin/updates/$BUILD
+SRC="p2updatesite/target/repository"
+TRG="/home/data/httpd/download.eclipse.org/modeling/emft/henshin/updates/$BUILD"
 
 rm -R $TRG/* 2> /dev/null
 cp -R $SRC/* $TRG
