eclipse-henshin/henshin

commit 3c62dadc6053d40fffaedf198bcd2227749ec559

The interpreter wizard does not allow to specify null arguments to the parameters for basic data types. Null arguments are required if the parameters should be bound by the rule.
This quick solution sets null arguments if the provided textfield is
either "" or "null". A proper solution would include an extra field for
string arguments to differentiate between "null" and null.

Change-Id: Ifeb1ee789f3f286355ea5c63b7944224d65c75d7

plugins/org.eclipse.emf.henshin.interpreter.ui/src/org/eclipse/emf/henshin/interpreter/ui/wizard/ParameterEditTable.java
@@ -177,25 +177,59 @@ public class ParameterEditTable {
 					try {
 						switch (paramCfg.getType()) {
 							case ParameterConfig.STRING:
-								paramCfg.setValue(value.toString());
-								break;
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(value.toString());
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 							case ParameterConfig.FLOAT:
-								paramCfg.setValue(Float.parseFloat(value.toString()));
-								break;
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(Float.parseFloat(value.toString()));
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 							case ParameterConfig.DOUBLE:
-								paramCfg.setValue(Double.parseDouble(value.toString()));
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(Double.parseDouble(value.toString()));
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 								break;
 							case ParameterConfig.INT:
-								paramCfg.setValue(Integer.parseInt(value.toString()));
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(Integer.parseInt(value.toString()));
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 								break;
 							case ParameterConfig.LONG:
-								paramCfg.setValue(Long.parseLong(value.toString()));
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(Long.parseLong(value.toString()));
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 								break;
 							case ParameterConfig.BOOLEAN:
-								paramCfg.setValue((Integer) value > 0 ? true : false);
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue((Integer) value > 0 ? true : false);
+								}
+								else {
+									paramCfg.setValue(null);
+								}	
 								break;
 							default:
-								paramCfg.setValue(value);
+								// should this case not simply throw an exception?
+								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+									paramCfg.setValue(value);
+								}
+								else {
+									paramCfg.setValue(null);
+								}
 						}
 						for (ParameterChangeListener l : listeners)
 							l.parameterChanged(paramCfg);

commit 3436455e74098bfcfd7e7f1a57c76b2404c92f28

fix for bug introduced by 3c62dadc6053d40fffaedf198bcd2227749ec559
(missing break for String and Float cases)

Change-Id: Ib861fffc4b0cc81e928bfdd11c3ec89dbf631abf

plugins/org.eclipse.emf.henshin.interpreter.ui/src/org/eclipse/emf/henshin/interpreter/ui/wizard/ParameterEditTable.java
@@ -189,21 +189,23 @@ public class ParameterEditTable {
 					try {
 						switch (paramCfg.getType()) {
 							case ParameterConfig.STRING:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue(value.toString());
 								}
 								else {
 									paramCfg.setValue(null);
-								}	
+								}
+								break;
 							case ParameterConfig.FLOAT:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue(Float.parseFloat(value.toString()));
 								}
 								else {
 									paramCfg.setValue(null);
-								}	
+								}
+								break;
 							case ParameterConfig.DOUBLE:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue(Double.parseDouble(value.toString()));
 								}
 								else {
@@ -211,7 +213,7 @@ public class ParameterEditTable {
 								}	
 								break;
 							case ParameterConfig.INT:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue(Integer.parseInt(value.toString()));
 								}
 								else {
@@ -219,7 +221,7 @@ public class ParameterEditTable {
 								}	
 								break;
 							case ParameterConfig.LONG:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue(Long.parseLong(value.toString()));
 								}
 								else {
@@ -227,7 +229,7 @@ public class ParameterEditTable {
 								}	
 								break;
 							case ParameterConfig.BOOLEAN:
-								if (! value.toString().trim().equals("") || value.toString().trim().equals("null")) {
+								if (! (value.toString().trim().equals("") || value.toString().trim().equals("null"))) {
 									paramCfg.setValue((Integer) value > 0 ? true : false);
 								}
 								else {