4ff28bb269a0c96532c594e0e01a6bed58c592e0
Daniel Strüber <strueber@mathematik.uni-marburg.de>
2018-02-20 15:47:34 +0100

    Interpreter wizard: Fix call of EmfCompare UI to support dynamic EMF
    Addresses bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=511567
    
    The call to the EmfCompare UI is now based on the usage example at
    https://www.eclipse.org/emf/compare/documentation/latest/developer/how-to-open-compare-dialog.html 
    
    The previous error (ClassNotFoundException) doesn't show anymore, since
    this code takes care of creating suitable ResourceSets for resolting the
    meta-model references.
    
    Change-Id: I09a1df6042091922f306483d68bd964769aaad36

--- plugins/org.eclipse.emf.henshin.interpreter.ui/src/org/eclipse/emf/henshin/interpreter/ui/wizard/HenshinWizard.java
+++ plugins/org.eclipse.emf.henshin.interpreter.ui/src/org/eclipse/emf/henshin/interpreter/ui/wizard/HenshinWizard.java
@@ -14,18 +14,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.compare.internal.CompareAction;
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareEditorInput;
+import org.eclipse.compare.CompareUI;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.domain.ICompareEditingDomain;
+import org.eclipse.emf.compare.domain.impl.EMFCompareEditingDomain;
+import org.eclipse.emf.compare.ide.ui.internal.configuration.EMFCompareConfiguration;
+import org.eclipse.emf.compare.ide.ui.internal.editor.ComparisonScopeEditorInput;
+import org.eclipse.emf.compare.match.DefaultComparisonFactory;
+import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
+import org.eclipse.emf.compare.match.DefaultMatchEngine;
+import org.eclipse.emf.compare.match.IComparisonFactory;
+import org.eclipse.emf.compare.match.IMatchEngine;
+import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryImpl;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryRegistryImpl;
+import org.eclipse.emf.compare.scope.DefaultComparisonScope;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.compare.utils.UseIdentifiers;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.henshin.interpreter.ui.HenshinInterpreterUIPlugin;
 import org.eclipse.emf.henshin.interpreter.ui.util.ParameterConfig;
 import org.eclipse.emf.henshin.interpreter.ui.util.TransformOperation;
@@ -37,15 +58,13 @@ import org.eclipse.emf.henshin.model.Parameter;
 import org.eclipse.emf.henshin.model.Unit;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Gregor Bonifer, Stefan Jurack, Christian Krause
  */
+@SuppressWarnings("restriction")
 public class HenshinWizard extends Wizard implements UnitSelectionListener,
 		ModelSelectorListener, ParameterChangeListener {
 
@@ -262,22 +281,33 @@ public class HenshinWizard extends Wizard implements UnitSelectionListener,
 		try {
 			getContainer().run(true, true, transformOperation);
 
-			if (page.openCompare.getSelection()) {
-				IFile left = getFile(page.outputSelector.getModelURI());
-				IFile right = getFile(page.inputSelector.getModelURI());
-				if (left != null && right != null) {
-					try {
-						ISelection selection = new StructuredSelection(
-								new Object[] { left, right });
-						MyCompareAction c = new MyCompareAction();
-						c.setActivePart(null, PlatformUI.getWorkbench()
-								.getActiveWorkbenchWindow().getActivePage()
-								.getActivePart());
-						c.run(selection);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
+			if (page.openCompare.getSelection()) {	// Load the two input models
+				ResourceSet resourceSet1 = new ResourceSetImpl();
+				ResourceSet resourceSet2 = new ResourceSetImpl();
+				String xmi1 = page.outputSelector.getModelURI();
+				String xmi2 = page.inputSelector.getModelURI();
+				load(xmi1, resourceSet1);
+				load(xmi2, resourceSet2);
+
+				// Configure EMF Compare
+				IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
+				IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
+				IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
+			        matchEngineFactory.setRanking(20);
+			        IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
+			        matchEngineRegistry.add(matchEngineFactory);
+				EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
+
+				// Compare the two models
+				IComparisonScope scope = new DefaultComparisonScope(resourceSet1, resourceSet2, null);
+			    ICompareEditingDomain editingDomain = EMFCompareEditingDomain.create(resourceSet1, resourceSet2, null);
+			    AdapterFactory adapterFactory = new ComposedAdapterFactory(
+				ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+			    
+				CompareEditorInput input = new ComparisonScopeEditorInput(new EMFCompareConfiguration(new CompareConfiguration()), 
+			        editingDomain, adapterFactory, comparator, scope);
+
+			    CompareUI.openCompareEditor(input);
 			}
 
 		} catch (InvocationTargetException e) {
@@ -293,14 +323,16 @@ public class HenshinWizard extends Wizard implements UnitSelectionListener,
 		return true;
 	}
 
-	private class MyCompareAction extends CompareAction {
-		@Override
-		public void run(ISelection selection) {
-			if (super.isEnabled(selection)) {
-				super.run(selection);
-			}
-		}
+	private void load(String absolutePath, ResourceSet resourceSet) {
+		URI uri = URI.createURI(absolutePath);
+
+		System.out.println(uri);
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+
+		// Resource will be loaded within the resource set
+		resourceSet.getResource(uri, true);
 	}
+	
 
 	/*
 	 * (non-Javadoc)
