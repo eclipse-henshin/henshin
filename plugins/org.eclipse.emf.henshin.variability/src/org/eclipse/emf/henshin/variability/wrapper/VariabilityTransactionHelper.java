4f8d84a9dd3662cd59367764e497afd31a8aca89
Stefan Schulz <schulzs@mathematik.uni-marburg.de>
2019-11-25 20:52:50 +0100

    Discard transaction exceptions when handling variability annotation.
    Change-Id: I2d71b5c0962fd2aeb7e304d1dffaa9dd151cc67c

--- plugins/org.eclipse.emf.henshin.variability/src/org/eclipse/emf/henshin/variability/wrapper/VariabilityTransactionHelper.java
+++ plugins/org.eclipse.emf.henshin.variability/src/org/eclipse/emf/henshin/variability/wrapper/VariabilityTransactionHelper.java
@@ -15,7 +15,8 @@ import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 
 /**
- * This cladss offers an interface to allow UI elements to modify the underlying models. 
+ * This cladss offers an interface to allow UI elements to modify the underlying
+ * models.
  * 
  * @author Stefan Schulz
  *
@@ -33,28 +34,43 @@ public class VariabilityTransactionHelper {
 
 		anno.setKey(key);
 		anno.setValue(value);
-
-		Command command = AddCommand.create(domain, modelElement, HenshinPackage.Literals.ANNOTATION, anno);
-		CommandStack stack = domain.getCommandStack();
-		stack.execute(command);
-
+		
+		try  {
+			modelElement.getAnnotations().add(anno);
+		} catch(IllegalStateException e) {
+		}
 		return anno;
 	}
 
 	public static void setAnnotationValue(ModelElement modelElement, String key, String value) {
-		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(modelElement);
-		if (domain == null) {
-			ResourceSet resSet = new ResourceSetImpl();
-			resSet.createResource(modelElement.eResource().getURI());
-			domain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain(resSet);
-		}
-		for (Annotation anno : modelElement.getAnnotations()) {
-			if (anno.getKey().equals(key)) {
-				Command command = SetCommand.create(domain, anno, HenshinPackage.Literals.ANNOTATION__VALUE, value);
-				CommandStack stack = domain.getCommandStack();
+		Annotation anno = getAnnotation(modelElement, key);
+
+		if (anno != null && anno.getValue() != null && !anno.getValue().equals(value)) {
+			TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(modelElement);
+			if (domain == null) {
+				ResourceSet resSet = new ResourceSetImpl();
+				resSet.createResource(modelElement.eResource().getURI());
+				domain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain(resSet);
+			}
+			Command command = SetCommand.create(domain, anno, HenshinPackage.Literals.ANNOTATION__VALUE, value);
+			CommandStack stack = domain.getCommandStack();
+			
+			try {
 				stack.execute(command);
+			} catch (IllegalStateException e) {
 			}
 		}
+	}
+
+	private static Annotation getAnnotation(ModelElement modelElement, String key) {
+		if (modelElement.getAnnotations() != null) {
+			for (Annotation anno : modelElement.getAnnotations()) {
+				if (anno.getKey().equals(key)) {
+					return anno;
+				}
+			}
+		}
+		return null;
 	}
 
 }
