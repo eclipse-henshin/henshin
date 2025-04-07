8456fcd98035777845457e698b353ad8d54d729d
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-04-07 17:49:27 +0200

    Remove generated xtend-gen files from SCM
    Instead of having generated files checked in to SCM, just generate them
    in the build and IDE.
    Because having them in the SCM can cause dirty workspaces in case these
    files are generated slightly different a workspace.
    
    Also remove empty 'src' and 'xtend-gen' folders from
    'org.eclipse.emf.henshin.text.ide' project.

--- plugins/org.eclipse.emf.henshin.text/xtend-gen/org/eclipse/emf/henshin/text/typesystem/Henshin_textNumberType.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.emf.henshin.text.typesystem;
-
-@SuppressWarnings("all")
-public class Henshin_textNumberType implements Henshin_textType {
-  public String toString() {
-    return "number";
-  }
-}
--- plugins/org.eclipse.emf.henshin.text/xtend-gen/org/eclipse/emf/henshin/text/typesystem/Henshin_textType.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.emf.henshin.text.typesystem;
-
-@SuppressWarnings("all")
-public interface Henshin_textType {
-  String toString();
-}
